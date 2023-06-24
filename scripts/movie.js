533c532a793cb57142d66213cb3af349b35743d6
sthika <sveta.tyo45@gmail.com>
2023-06-24 17:15:39 +0600

    fixed films cards

--- scripts/movie.js
+++ scripts/movie.js
@@ -3,21 +3,22 @@ let swiperWrapper = document.querySelector(".swiper-wrapper")
 // create cards with "swiper-slide" class
     
 const options = {method: 'GET', headers: {accept: 'application/json'}};
-let card = document.createElement("div")
-let imageWrap = document.createElement("div")
-let img = document.createElement("img")
+
 fetch('https://api.themoviedb.org/3/discover/movie?language=ru-RUS&api_key=baacee587b52679e93f67d12424c4cb3', options)
     .then(response => response.json())
      .then((response) => {
         console.log(response)
         return response["results"].forEach(el => {
             const drawCard = () => {
+                let card = document.createElement("div")
                 card.classList = "swiper-slide"
                 swiperWrapper.appendChild(card)
         
+                let imageWrap = document.createElement("div")
                 imageWrap.classList = "swiper__img"
                 card.appendChild(imageWrap)
                 
+                let img = document.createElement("img")
                 imageWrap.appendChild(img)
                 img.src = `https://image.tmdb.org/t/p/w500${el["poster_path"]}` 
 
@@ -33,4 +34,3 @@ fetch('https://api.themoviedb.org/3/discover/movie?language=ru-RUS&api_key=baace
      })
     .catch(err => console.error(err));
 
-let cardModal = 
